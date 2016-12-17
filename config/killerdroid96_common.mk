25103a52eeeeafc56fcbf322d9fdc2d7f8b57053
beanstown106 <nbenis106@gmail.com>
2016-12-17 03:11:42 -0500

    Add Pixel wallpapers
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- config/killerdroid96_common.mk
+++ config/killerdroid96_common.mk
@@ -11,8 +11,28 @@ PRODUCT_PACKAGES += \
     SnapdragonGallery \
     SnapdragonCamera \
     Spectrum \
-    Turbo \
+    Turbo 
+
+# Prebuilt applications
+PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     NexusWallpapersStubPrebuilt \
     WallpaperPickerGooglePrebuilt \
     WallpapersUsTwo
+
+# wallpaper location prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.wallpapers_loc_request_suw=true
+
+# live wallpapers arm64 only
+ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_DEVICE)),)
+    PRODUCT_PACKAGES += \
+        WallpapersBReel \
+        libgdx.so \
+        libgeswallpapers-jni.so \
+        libjpeg.so
+
+    PRODUCT_COPY_FILES += \
+        vendor/flash/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
+        vendor/flash/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
+endif
