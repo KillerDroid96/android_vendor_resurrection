e104a4751ce2620d5e2234ea8329351436e64380
KillerDroid96 <mkeller96@gmail.com>
2017-03-27 21:12:06 +0000

    Reduced countdown to 5sec Reduced default days to changelog to 3 Set Offical as default varaint

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -46,12 +46,13 @@ if [ -z $days_to_log ];then
 echo ""
 echo ${ylw}" ▼ For how many days changelog do you want to generate?"${txtrst}
 echo ""
-echo -e ${blu}" 🕑  30/sec Time-out "${txtrst}${red}"︱ 7 Days by default"${txtrst}
+echo -e ${blu}" 5/sec Time-out "${txtrst}${red}"︱ 3 Days by default"${txtrst}
 echo -e "";
 echo -e ${cya}" ▼ Type a number"${txtrst}
 echo -e "";
 # use 'export days_to_log=5' before '. build/envsetup.sh' were 5 is days to log
-read -r -t 30 days_to_log || days_to_log=7
+if [ -z $days_to_log ];then
+read -r -t 5 days_to_log || days_to_log=3
 fi
 echo >> $Changelog;
 echo " ▼ $source_name Ver 5.8.3 Changelog"    >> $Changelog;
