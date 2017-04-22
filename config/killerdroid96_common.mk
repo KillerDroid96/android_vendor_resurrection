d631a996f358e6a9c2b820df9b9e4f57d68c1d0e
DirtyDragons <KillerDroid96@users.noreply.github.com>
2017-04-22 00:01:21 -0500

    Update killerdroid96_common.mk

--- config/killerdroid96_common.mk
+++ config/killerdroid96_common.mk
@@ -42,6 +42,6 @@ PRODUCT_COPY_FILES += \
    vendor/cm/prebuilt/ramdisk/init.flash.rc:root/init.flash.rc \
    vendor/cm/prebuilt/ramdisk/init.profiles.rc:root/init.profiles.rc \
    vendor/cm/prebuilt/ramdisk/init.profiles.sh:root/init.profiles.sh \
-   vendor/cm/prebuilt/ramdisk/angler/init.special_power.sh:root/init.special_power.sh \
+   vendor/cm/prebuilt/ramdisk/init.special_power.sh:root/init.special_power.sh \
    vendor/cm/prebuilt/ramdisk/msm_irqbalance.conf:root/msm_irqbalance.conf
  
