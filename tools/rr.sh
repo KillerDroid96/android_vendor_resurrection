443d88faf1d753089a25006c467b612a9c44b41c
KillerDroid96 <mkeller96@gmail.com>
2017-04-28 16:04:29 -0500

    Custom ROM Build Output

--- tools/rr.sh
+++ tools/rr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Root method chooser for the Resurrection Remix ROM
-# Specify colors utilized in the terminal
+# Specify colors for shell
     red=$(tput setaf 1)             #  red
     grn=$(tput setaf 2)             #  green
     ylw=$(tput setaf 3)             #  yellow
@@ -24,37 +23,34 @@
 
 echo -e "";
 echo -e "";
-echo -e ${blu}"              GGGGGGGGGGGGG       OOOOOOOOO       TTTTTTTTTTTTTTTTTTTTTTT"${txtrst};
-echo -e ${blu}"           GGG::::::::::::G     OO:::::::::OO     T:::::::::::::::::::::T"${txtrst};
-echo -e ${blu}"         GG:::::::::::::::G   OO:::::::::::::OO   T:::::::::::::::::::::T"${txtrst};
-echo -e ${grn}"        G:::::GGGGGGGG::::G  O:::::::OOO:::::::O  T:::::TT:::::::TT:::::T"${txtrst};
-echo -e ${grn}"       G:::::G       GGGGGG  O::::::O   O::::::O  TTTTTT  T:::::T  TTTTTT"${txtrst};
-echo -e ${ylw}"      G:::::G                O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${ylw}"      G:::::G                O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${ylw}"      G:::::G    GGGGGGGGGG  O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${red}"      G:::::G    G::::::::G  O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${red}"      G:::::G    GGGGG::::G  O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${red}"      G:::::G        G::::G  O:::::O     O:::::O          T:::::T        "${txtrst};
-echo -e ${ppl}"       G:::::G       G::::G  O::::::O   O::::::O          T:::::T        "${txtrst};
-echo -e ${ppl}"        G:::::GGGGGGGG::::G  O:::::::OOO:::::::O        TT:::::::TT      "${txtrst};
-echo -e ${ppl}"         GG:::::::::::::::G   OO:::::::::::::OO         T:::::::::T      "${txtrst};
-echo -e "           GGG::::::GGG:::G     OO:::::::::OO           T:::::::::T      ";
-echo -e "              GGGGGG   GGGG       OOOOOOOOO             TTTTTTTTTTT  ";
+tput setaf 1
+echo "	 ______                                                _             ";
+echo "	(_____ \                                           _  (_)            ";
+echo "	 _____) )_____  ___ _   _  ____ ____ _____  ____ _| |_ _  ___  ____  ";
+echo "	|  __  /| ___ |/___) | | |/ ___) ___) ___ |/ ___|_   _) |/ _ \|  _ \ ";
+echo "	| |  \ \| ____|___ | |_| | |  | |   | ____( (___  | |_| | |_| | | | |";
+echo "	|_|   |_|_____|___/|____/|_|  |_|   |_____)\____)  \__)_|\___/|_| |_|";
+echo "	                                                                     ";
+tput setaf 3
+echo "		     ______              _          _______ ______               ";
+echo "		    (_____ \            (_)        (_______) _____)              ";
+echo "		     _____) )_____ ____  _ _   _    _     ( (____                ";
+echo "		    |  __  /| ___ |    \| ( \ / )  | |   | \____ \               ";
+echo "		    | |  \ \| ____| | | | |) X (   | |___| |____) )              ";
+echo "		    |_|   |_|_____)_|_|_|_(_/ \_)   \_____(______/               ";
+echo "	                                                                     ";
+tput setaf 2
+echo "	    ______                                     ___       _  _ _ _    ";
+echo "	   / _____)                                   / __)     | || | | |   ";
+echo "	  ( (____  _   _  ____ ____ _____  ___  ___ _| |__ _   _| || | | |   ";
+echo "	   \____ \| | | |/ ___) ___) ___ |/___)/___|_   __) | | | ||_|_|_|   ";
+echo "	   _____) ) |_| ( (__( (___| ____|___ |___ | | |  | |_| | | _ _ _    ";
+echo "	  (______/|____/ \____)____)_____|___/(___/  |_|  |____/ \_)_|_|_|   ";
+echo "	                                                                     ";
+tput setaf 4
+echo -e " 〉Powered by ANDROID ™  // NOUGAT 7.1.2";
 echo -e "";
-echo -e "";
-echo -e "   88888  88888 888888 8   8 88888  88888  88888 888888 88888 88888 888888 ";
-echo -e ${grn}"   8   8  8     8      8   8 8   8  8   8  8     8    8   8   8     8    8 "${txtrst};
-echo -e ${ylw}"   8eee8e 8eeee 8eeeee 8e  8 8eee8e 8eee8e 8eeee 8e       8e  8eeee 8e   8 "${txtrst};
-echo -e ${red}"   88   8 88        88 88  8 88   8 88   8 88    88       88  88    88   8 "${txtrst};
-echo -e ${blu}"   88   8 88    e   88 88  8 88   8 88   8 88    88   e   88  88    88   8 "${txtrst};
-echo -e ${ppl}"   88   8 88eee 8eee88 88ee8 88   8 88   8 88eee 88eee8   88  88eee 88eee8 "${txtrst};
-echo -e "";
-echo -e "";
-echo -e "";
-echo -e "";
-echo -e ${ylw}" √ Build is Successfully Done! "${txtrst};
-echo -e "";
-echo -e ${blu}" 〉Powered by ANDROID ™  // NOUGAT"${txtrst};
-echo -e "";
-echo -e ${red}" 〉Enjoy the Resurrection Remix OS // #GETRESURRECTED "${txtrst};
+tput setaf 1
+echo -e " 〉Enjoy the Resurrection Remix OS // #GETRESURRECTED ";
+tput sgr0
 echo -e "";
