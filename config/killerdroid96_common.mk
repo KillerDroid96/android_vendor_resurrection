cc33b8350903cf8d9f5a1a0678665946f2e98081
DirtyDragons <KillerDroid96@users.noreply.github.com>
2017-05-05 16:53:34 -0500

    Update killerdroid96_common.mk

--- config/killerdroid96_common.mk
+++ config/killerdroid96_common.mk
@@ -15,13 +15,14 @@ PRODUCT_PACKAGES += \
     SnapdragonGallery \
     SnapdragonCamera \
     Spectrum \
-    Turbo 
+    Turbo
+    
 # Copy over our ramdisk files
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/ramdisk/init.flash.rc:root/init.flash.rc \
-    vendor/cm/prebuilt/ramdisk/init.profiles.rc:root/init.profiles.rc \
-    vendor/cm/prebuilt/ramdisk/init.profiles.sh:root/init.profiles.sh \
-    vendor/cm/prebuilt/ramdisk/init.special_power.sh:root/init.special_power.sh \
-    vendor/cm/prebuilt/ramdisk/msm_irqbalance.conf:root/msm_irqbalance.conf
+#PRODUCT_COPY_FILES += \
+#    vendor/cm/prebuilt/ramdisk/init.flash.rc:root/init.flash.rc \
+#    vendor/cm/prebuilt/ramdisk/init.profiles.rc:root/init.profiles.rc \
+#    vendor/cm/prebuilt/ramdisk/init.profiles.sh:root/init.profiles.sh \
+#    vendor/cm/prebuilt/ramdisk/init.special_power.sh:root/init.special_power.sh \
+#    vendor/cm/prebuilt/ramdisk/msm_irqbalance.conf:root/msm_irqbalance.conf
 
  
