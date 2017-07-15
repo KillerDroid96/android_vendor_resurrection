203476a0f884276fea486bfbf88212bb16c88c76
KillerDroid96 <KillerDroid96@users.noreply.github.com>
2017-07-15 12:59:11 -0500

    Back to basics for now

--- config/killerdroid96_common.mk
+++ config/killerdroid96_common.mk
@@ -1,41 +1,41 @@
 #KillerDroid96 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.user=KillerDroid96 \
-    rr.build.type=Custom 
+    ro.build.user=KillerDroid96 
+    #rr.build.type=Custom 
 
 # My Addons
-PRODUCT_PACKAGES += \
-    AdAway \
-    KernelAdiutor \
-    SnapBrowser \
-    SnapdragonGallery \
-    SnapdragonCamera \
-    Spectrum \
-    Turbo 
+# PRODUCT_PACKAGES += \
+   # AdAway \
+   # KernelAdiutor \
+   # SnapBrowser \
+   # SnapdragonGallery \
+   # SnapdragonCamera \
+   # Spectrum \
+   # Turbo 
 
 # Prebuilt applications
-PRODUCT_PACKAGES += \
-    LiveWallpapersPicker \
-    NexusWallpapersStubPrebuilt \
-    WallpaperPickerGooglePrebuilt \
-    WallpapersUsTwo
+# PRODUCT_PACKAGES += \
+#    LiveWallpapersPicker \
+#    NexusWallpapersStubPrebuilt \
+#    WallpaperPickerGooglePrebuilt \
+#    WallpapersUsTwo
 
 # wallpaper location prop
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.wallpapers_loc_request_suw=true
+# PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.wallpapers_loc_request_suw=true
 
 # live wallpapers arm64 only
-ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_DEVICE)),)
-    PRODUCT_PACKAGES += \
-        WallpapersBReel \
-        libgdx.so \
-        libgeswallpapers-jni.so \
-        libjpeg.so
+## ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_DEVICE)),)
+#    PRODUCT_PACKAGES += \
+#        WallpapersBReel \
+ #       libgdx.so \
+ #       libgeswallpapers-jni.so \
+#        libjpeg.so
 
-    PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
-        vendor/cm/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
-endif
+ #   PRODUCT_COPY_FILES += \
+ #       vendor/cm/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
+#        vendor/cm/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
+# endif
 
 # Copy over our ramdisk files
 PRODUCT_COPY_FILES += \
